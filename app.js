420a4f14825897892d9a77b21b065a43a48883af
Edith Li <Edith Li>
2016-09-14 23:40:29 -0400

    "complete app.js "

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
 var Engine = require('tingodb')();
+
 var database = new Engine.Db(__dirname + '/db',{});
+
 var sampleCollection = database.collection('somestuff');
-sampleCollection.insert({
-   "sensorvalue" : Math.random() * 100,
-   "datetime" : new Date()
-});
+
+setInterval(function(){
+	var sampleCollection = database.collection('somestuff');
+	sampleCollection.insert({
+		"sensorvalue" : Math.random() * 100,
+		"datetime" : new Date()
+	});
+	console.log("added a sample");
+},1000);
+
+var getLatestSamples = function(theCount,callback){
+	var sampleCollection = database.collection('somestuff');
+	sampleCollection
+		.find()
+		.sort({"datetime":-1})
+		.limit(theCount)
+		.toArray(function(err,docList){
+			callback(docList);
+		});
+};
+
+setInterval(function(){
+	getLatestSamples(5,function(results){
+		var theValues = []
+		for(var i=0; i<results.length; i++)
+		{
+			theValues.push(results[i].sensorvalue);
+		}
+		console.log(theValues);
+	});
+},3000);
